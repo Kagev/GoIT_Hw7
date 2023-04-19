a2a98cf62a51fce1f9416dadb1fd68e1ae219322
Kagev <kaplitskiygv@gmail.com>
2023-04-19 11:39:26 +0300

    Ok

---
+++ clean-folder/clean-folder/normalize.py
@@ -0,0 +1,21 @@
+import re
+import os
+
+CYRILLIC_SYMBOLS = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ'
+TRANSLATION = ("a", "b", "v", "g", "d", "e", "e", "j", "z", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u",
+               "f", "h", "ts", "ch", "sh", "sch", "", "y", "", "e", "yu", "u", "ja", "je", 'i', "ji", "g")
+
+
+TRANS = {}
+
+for c, l in zip(CYRILLIC_SYMBOLS, TRANSLATION):
+    TRANS[ord(c)] = l
+    TRANS[ord(c.upper())] = l.upper()
+
+
+def normalize(name: str) -> str:
+    t_name = name.translate(TRANS)
+    name, extension = os.path.splitext(t_name)
+    name = re.sub(r'\W', '_', name)
+    t_name = name + extension
+    return t_name
